6c83d9b0a5873587e2774d65cdcd21a99c0ce48b
Chalermchai <ebiz_niit5@scg.com>
2019-05-22 08:49:46 +0700

    make a simple auth app

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 module.exports = {
-  //   DB: "mongodb://localhost:27017/auth"
   DB:
-    "mongodb+srv://miniadmin:UQ3qmK1X9cfy2knu@cluster0-tzjxz.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://miniAdmin:nJQ7nPBOpAw5bch7@cluster0-20dij.mongodb.net/test?retryWrites=true"
 };
